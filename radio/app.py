7921aab290d51d16cba86441c74c071191de79d4
kali <kali@kali.kali>
2023-03-12 14:31:11 +0300

    first commit

--- radio/app.py
+++ radio/app.py
@@ -1,28 +1,33 @@
-from flask import Flask, request
-import numpy as np
-import scipy.io.wavfile as wav
-
-from flask import  render_template
-
-# import the Python script for processing the audio file
-
+from flask import Flask, render_template
+import pyaudio
+import serial
 
 app = Flask(__name__)
 
-# define a route for the HTML page
+# Set up the PyAudio and PySerial objects
+chunk = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+ser = serial.Serial('/dev/tty.usbserial', 9600) # Replace with the serial port your radio transceiver is connected to
+
+p = pyaudio.PyAudio()
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=chunk)
+
 @app.route('/')
 def index():
-    return render_template('teach.html')
+    return render_template('radio.html')
 
-# define a route for processing the uploaded audio file
-@app.route('/process_audio', methods=['POST'])
-def process_audio():
-    audio_file = request.files['audio_file']
-    audio_data = audio_file.read()
-    signal = audio_to_signal(audio_data)
-    # do something with the signal data
-    return 'Audio file processed successfully'
+@app.route('/transmit')
+def transmit():
+    # Continuously read audio from the microphone and transmit it through the radio transceiver
+    while True:
+        data = stream.read(chunk)
+        ser.write(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
